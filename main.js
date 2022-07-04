da388c737006a4954e205bbc8097f572b50211f2
Valerio Genco <valerio.gc1@gmail.com>
2022-07-04 17:35:41 +0200

    Revisione aggiunta decimali

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //Dichiaro variabili KM ed Età 
-let distance = parseInt(prompt("Quanti Kilometri devi percorrere?"));
+let distance = prompt("Quanti Kilometri devi percorrere?");
 console.log(`L'utente deve percorrere ${distance}km`);
 if (isNaN(distance)) {
     alert("Il valore inserito non è un numero! Per favore inserisci correttamente i valori richiesti");
@@ -15,24 +15,30 @@ else {
     else {
         //Calcolo prezzi biglietti
         const full_price_ticket = ((distance * 0.21) * 100) / 100;
+        const ticket = Math.ceil(full_price_ticket * 100) / 100;
         console.log(`Il prezzo intero del biglietto è: ${full_price_ticket}€`);
+        console.log(`Il prezzo intero con decimali del biglietto è: ${ticket}€`);
         // Biglietto sconto over
-        const discount_under = ((full_price_ticket * 20) / 100);
+        let discount_under = ((full_price_ticket * 20) / 100); //calcolo sconto 20%
         let full_price_ticket_under = full_price_ticket - discount_under;
+        let discount_ticket_under = Math.round(full_price_ticket_under * 100) / 100;
         console.log(`Il prezzo under 18 del biglietto è: ${full_price_ticket_under}€`);
+        console.log(`Il prezzo under 18 con decimali del biglietto è: ${discount_ticket_under}€`);
         // Biglietto sconto over
-        const discount_over =((full_price_ticket * 40) / 100);
+        let discount_over = ((full_price_ticket * 40) / 100); //calcolo sconto 40%
         let full_price_ticket_over = full_price_ticket - discount_over;
+        let discount_ticket_over = Math.ceil(full_price_ticket_over * 100) / 100;
         console.log(`Il prezzo over 65 del biglietto è: ${full_price_ticket_over}€`);
+        console.log(`Il prezzo over 65 con decimali del biglietto è: ${discount_ticket_over}€`);
         // Output prezzo biglietto del treno
         if (age <= 18) {
-            alert(`Hai diritto ad uno sconto per under 18. Il prezzo del biglietto è di ${full_price_ticket_under}€`);
+            alert(`Hai diritto ad uno sconto per under 18! Il prezzo del biglietto è di ${discount_ticket_over}€`);
         } 
         else if (age >= 65) {
-            alert(`Hai diritto ad uno sconto per over 65. Il prezzo del biglietto è di ${full_price_ticket_over}€`);
+            alert(`Hai diritto ad uno sconto per over 65! Il prezzo del biglietto è di ${discount_ticket_over}€`);
         }
         else {
-            alert(`Il prezzo del biglietto è di ${full_price_ticket}€`);
+            alert(`Il prezzo del biglietto è di ${ticket}€`);
         }
     }
 }
